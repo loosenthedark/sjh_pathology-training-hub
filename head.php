b2fbe69074d216ed84f35a7c19df0b6312a79ac0
Paul Harrington <hello@loosenthedark.tech>
2021-10-01 17:11:43 +0100

    Add favicons and meta tag info to head.php

--- head.php
+++ head.php
@@ -1,17 +1,26 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<meta http-equiv="X-UA-Compatible" content="ie=edge">
-<meta name="Description" content="Enter your description here"/>
-<!-- Font Awesome CSS -->
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.css" integrity="sha512-4wfcoXlib1Aq0mUtsLLM74SZtmB73VHTafZAvxIp/Wk9u1PpIsrfmTvK0+yKetghCL8SHlZbMyEcV8Z21v42UQ==" crossorigin="anonymous" referrerpolicy="no-referrer">
-<!-- Bootstrap CSS -->
-<!-- <link rel="stylesheet" href="static/css/libs/bootstrap.min.css"> -->
-<!-- Bootstrap 4 Vertical Tabs CSS extension -->
-<link rel="stylesheet" href="static/css/libs/b4vtabs.min.css">
-<!-- custom stylesheet -->
-<link rel="stylesheet" href="static/css/style.css">
-<title>Pathology Training Hub | St James's Hospital</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="apple-touch-icon" sizes="180x180" href="static/images/icons/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="static/images/icons/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="static/images/icons/favicon/favicon-16x16.png">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!--
+      manifest.json provides metadata used when your web app is installed on a
+      user's mobile device or desktop. See https://developers.google.com/web/fundamentals/web-app-manifest/
+    -->
+    <link rel="manifest" href="manifest.json" />
+    <meta name="Description" content="Enter your description here" />
+    <!-- Font Awesome CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.css" integrity="sha512-4wfcoXlib1Aq0mUtsLLM74SZtmB73VHTafZAvxIp/Wk9u1PpIsrfmTvK0+yKetghCL8SHlZbMyEcV8Z21v42UQ==" crossorigin="anonymous" referrerpolicy="no-referrer">
+    <!-- Bootstrap CSS -->
+    <!-- <link rel="stylesheet" href="static/css/libs/bootstrap.min.css"> -->
+    <!-- Bootstrap 4 Vertical Tabs CSS extension -->
+    <link rel="stylesheet" href="static/css/libs/b4vtabs.min.css">
+    <!-- custom stylesheet -->
+    <link rel="stylesheet" href="static/css/style.css">
+    <title>Pathology Training Hub | St James's Hospital</title>
 </head>
